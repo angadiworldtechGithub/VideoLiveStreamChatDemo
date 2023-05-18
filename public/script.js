85ca637a3dae24f1c60e74bdd633ad30d6f046b9
tejasj1718 <55607809+tejasj1718@users.noreply.github.com>
2023-05-18 17:39:12 +0530

    styling and server changes

--- public/script.js
+++ public/script.js
@@ -1,21 +1,24 @@
+let userVideo;
 let videoGrid;
 let myPeer;
 let myVideo;
 let myVideoStream;
 const peers = {};
+const streamCalled = {}; // Since streams are called twice
 const SOCKET_IO_PORT = 4000;
 
-// seperate chat from user
+// close peer connection when the browser closes
 
 $(() => {
   const socket = io(`ws://localhost:${SOCKET_IO_PORT}`);
+  userVideo = document.getElementById("user-video");
   videoGrid = document.getElementById("video-grid");
   myVideo = document.createElement("video");
   myVideo.muted = true;
 
   navigator.mediaDevices
     .getUserMedia({
-      video: true,
+      video: { width: 1200, height: 1000 },
       audio: true,
     })
     .then((stream) => {
@@ -27,17 +30,16 @@ $(() => {
         port: "3030",
       });
 
-      myPeer.on("call", (call) => {
-        call.answer(stream);
-        const video = document.createElement("video");
-        call.on("stream", (userVideoStream) => {
-          addVideoStream(video, userVideoStream);
-        });
-      });
-
       myPeer.on("open", (id) => {
         console.info(`User - ${id} is joining room ${ROOM_ID}`);
         socket.emit("join-room", ROOM_ID, id);
+      });
+
+      myPeer.on("call", (call) => {
+        console.log("Receiving a call");
+        call.answer(stream);
+        const video = document.createElement("video");
+        call.on("stream", streamHandler(video));
       });
 
       myPeer.on("error", (error) => console.error(error));
@@ -48,7 +50,7 @@ $(() => {
       });
 
       myVideoStream = stream;
-      addVideoStream(myVideo, stream);
+      addVideoStream(myVideo, stream, "user");
     })
     .catch((error) => {
       // code to show that the feed is broken.
@@ -77,14 +79,15 @@ $(() => {
 });
 
 function connectToNewUser(userId, stream) {
+  console.log("Connecting to New User Peer");
   if (myPeer) {
     const call = myPeer.call(userId, stream);
-    console.log(call);
     const video = document.createElement("video");
-    call.on("stream", (userVideoStream) => {
-      addVideoStream(video, userVideoStream);
-    });
+    call.on("stream", streamHandler(video));
+
     call.on("close", () => {
+      console.log("Call closing");
+      video.parentElement.remove();
       video.remove();
     });
     peers[userId] = call;
@@ -93,12 +96,29 @@ function connectToNewUser(userId, stream) {
   }
 }
 
-function addVideoStream(video, stream) {
+function addVideoStream(video, stream, type) {
   video.srcObject = stream;
   video.addEventListener("loadedmetadata", () => {
     video.play();
   });
-  videoGrid.append(video);
+
+  if (type === "user") {
+    userVideo.prepend(video);
+  } else if (type === "caller") {
+    const div = document.createElement("div");
+    div.classList.add("video-pip");
+    div.append(video);
+    videoGrid.append(div);
+  }
+}
+
+function streamHandler(video) {
+  return function (userVideoStream) {
+    if (!streamCalled[userVideoStream]) {
+      addVideoStream(video, userVideoStream, "caller");
+      streamCalled[userVideoStream] = true;
+    }
+  };
 }
 
 const scrollToBottom = () => {
@@ -128,9 +148,11 @@ const playStop = () => {
     if (enabled) {
       myVideoStream.getVideoTracks()[0].enabled = false;
       setPlayVideo();
+      // add user id inplace of video or do it via html
     } else {
       setStopVideo();
       myVideoStream.getVideoTracks()[0].enabled = true;
+      // remove user id inplace of video or do it via html
     }
   } else {
     console.error("Video not Enabled");
